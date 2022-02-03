372d6d685c845a4219de88eae888007e3f4d40ca
natnatjr <ejnatnat@yahoo.com>
2022-02-02 19:54:57 -0500

    I added 'Hearts' and '1000' as my arguements

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -7,13 +7,18 @@ package ca.sheridancollege.week2.softwarefundamentals;
 
 /**
  * The class where we create Cards
+ *
  * @author dancye, 2019
+ *
+ * Student Name: Edwin Natnat Jr
+ * Student Number: 991526343
  */
-public class CardDealer 
+public class CardDealer
 {
-    public static void main(String[] args)
-    {
-        Card nineClubs = new Card("Clbs", 9);
-        nineClubs.setSuit("diamonds");
-    }
+   public static void main (String[] args)
+   {
+      Card call = new Card("Hearts", 1000);
+      Card nineClubs = new Card("Clbs", 9);
+      nineClubs.setSuit("diamonds");
+   }
 }
